7b5b0c14896469c57380edabf9621af5f628f820
Erand12334 <erandkurtaliqi1@gmail.com>
2024-11-16 17:22:27 +0100

    Profile Page UI

---
+++ app/src/main/java/com/example/discussfirst/ChangePassword.java
@@ -0,0 +1,33 @@
+package com.example.discussfirst;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void showChangePasswordDialog(View view) {
+        // Create a dialog for password change
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.activity_dialog_change_password);
+        dialog.setCancelable(true); // Make it cancellable by touch outside
+        dialog.show();
+    }
+}
